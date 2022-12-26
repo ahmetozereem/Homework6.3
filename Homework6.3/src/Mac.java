dec148075c7c2fa15f573d937a259972f15121b1
Asus <Asus@A-S>
2022-12-26 14:28:08 +0100

    Homework6.3

---
+++ Homework6.3/src/Mac.java
@@ -0,0 +1,12 @@
+
+public class Mac {
+	
+	public int fiyat;
+	public String model;
+	public double ekranBoyutu;
+	
+	public Mac() {
+		super();
+	}
+
+}
---
+++ Homework6.3/src/Main.java
@@ -0,0 +1,57 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Mac mac1 = new Mac ();
+		
+		mac1.fiyat=1500;
+		mac1.ekranBoyutu=13;
+		mac1.model="MacBook Pro";
+		
+		System.out.println("" );
+		System.out.println("Mac1: "+"Fiyat: "+mac1.fiyat+" Euro" +"; "+"Model: "+mac1.model+"; "+"Ekran Boyutu: "+mac1.ekranBoyutu+" inch" );
+		System.out.println("" );
+		
+		Mac mac2 = new Mac ();
+		
+		mac2.fiyat=965;
+		mac2.ekranBoyutu=13.5;
+		mac2.model="Mac Air 2020";
+		
+		System.out.println("" );
+		System.out.println("Mac2: "+"Fiyat: "+mac2.fiyat+" Euro" +"; "+"Model: "+mac2.model+"; "+"Ekran Boyutu: "+mac2.ekranBoyutu+" inch" );
+		System.out.println("" );
+		
+		Mac mac3 = new Mac ();
+		
+		mac3.fiyat=1556;
+		mac3.ekranBoyutu=21.5;
+		mac3.model="Mac 4K";
+		
+		System.out.println("" );
+		System.out.println("Mac3: "+"Fiyat: "+mac3.fiyat+" Euro" +"; "+"Model: "+mac3.model+"; "+"Ekran Boyutu: "+mac3.ekranBoyutu+" inch" );
+		System.out.println("" );
+		
+		Mac mac4 = new Mac ();
+		
+		mac4.fiyat=5539;
+		mac4.ekranBoyutu=27;
+		mac4.model="iMac Pro";
+		
+		System.out.println("" );
+		System.out.println("Mac4: "+"Fiyat: "+mac4.fiyat+" Euro" +"; "+"Model: "+mac4.model+"; "+"Ekran Boyutu: "+mac4.ekranBoyutu+" inch" );
+		System.out.println("" );
+		
+		Mac mac5 = new Mac ();
+		
+		mac5.fiyat=1699;
+		mac5.ekranBoyutu=27;
+		mac5.model="iMac Retina 5K 2020";
+		
+		System.out.println("" );
+		System.out.println("Mac5: "+"Fiyat: "+mac5.fiyat+" Euro" +"; "+"Model: "+mac5.model+"; "+"Ekran Boyutu: "+mac5.ekranBoyutu+" inch" );
+		System.out.println("" );
+	}
+
+}
